0f26d0e11c24a2795e5b6d4776564b16c8a6299b
AJAY <chajay55555@gmail.com>
2024-12-05 23:57:31 -0600

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/chajaykrishna/go-ecommerce/controllers"
+	"github.com/chajaykrishna/go-ecommerce/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,6 +13,16 @@ func RegisterRoutes(app *fiber.App) {
 	app.Get("api/v1/validateUsername/:username", controllers.ValidateUsername)
 
 	app.Get("api/v1/login", controllers.Login)
+
+	app.Get("api/v1/myDetails", middlewares.ValidateJwtToken, controllers.FetchUserDetails)
+
+	app.Post("api/v1/addProduct", middlewares.ValidateJwtToken, controllers.AddProduct)
+
+	app.Get("api/v1/products", middlewares.ValidateJwtToken, controllers.GetAllProducts)
+
+	app.Get("api/v1/product/:{id}", middlewares.ValidateJwtToken, controllers.GetProductById)
+
+	app.Put("api/v1/product/:id", middlewares.ValidateJwtToken, controllers.UpdateProduct)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!")
